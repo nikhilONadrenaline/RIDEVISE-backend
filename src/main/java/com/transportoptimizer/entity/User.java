1058e2bb639d98648dc7037a774f78d0b15e7afd
NikhilOnAdrenaline <gamaxene1122@gmail.com>
2026-01-10 02:06:24 +0530

    Add User entity class for MongoDB

---
+++ src/main/java/com/transportoptimizer/entity/User.java
@@ -0,0 +1,20 @@
+package com.transportoptimizer.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "users")
+public class User {
+
+    @Id
+    private String id;
+
+    private String username;
+
+    private String password;
+}
